61dc459d9069e4c01248c0cf548f8729b08c578c
Nikulnikov Kirill <Nikulnikovki@rambler.ru>
2025-11-26 16:36:59 +0400

    add filters

--- NewsPortal/news/views.py
+++ NewsPortal/news/views.py
@@ -8,6 +8,7 @@ class PostListView(ListView):
     model = Post
     context_object_name = 'posts'
     template_name = 'news/post_list.html'
+    ordering = ['-dateCreation']
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
